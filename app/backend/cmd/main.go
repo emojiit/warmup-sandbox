4156e8b82e99c011c3afb2398fd8fe459916b2fd
zielo-hue <51804240+zielo-hue@users.noreply.github.com>
2021-08-25 22:44:19 -0700

    checksums lookin a little weird

--- app/backend/cmd/main.go
+++ app/backend/cmd/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"sandbox/cmd/serve"
 )
 
 func main() {
 	if os.Args[1] == "serve" {
+		fmt.Println("Serving...")
 		serve.Run()
 	}
 }
--- app/backend/cmd/serve/main.go
+++ app/backend/cmd/serve/main.go
@@ -1,19 +1,26 @@
 package serve
 
 import (
+	"flag"
 	"net/http"
 	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/render"
 )
+
+var routes = flag.Bool("routes", false, "Generate router documentation")
 
 func Run() {
 	r := chi.NewRouter()
 
 	// middleware
+	r.Use(middleware.RequestID)
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
+	r.Use(middleware.URLFormat)
+	r.Use(render.SetContentType(render.ContentTypeJSON))
 
 	// set timeout to 60 seconds
 	r.Use(middleware.Timeout(60 * time.Second))
@@ -23,4 +30,11 @@ func Run() {
 		w.Write([]byte("welcome!!!"))
 	})
 	http.ListenAndServe(":3000", r)
+
+	r.Route("/main", func(r chi.Router) {
+		r.Get("/", func(rw http.ResponseWriter, r *http.Request) {
+			rw.Write([]byte("test response"))
+		})
+	})
+
 }
